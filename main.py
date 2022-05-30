dc155ccdcc839f20f2d975eb27b27016d3f62e3f
Aziz Rosyid <azizrosyid783@gmail.com>
2022-05-30 19:28:53 +0700

    feat: add many feature

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import math
 import queue
 import tkinter
 import tkinter.filedialog
-from tkinter import (CENTER, LEFT, NW, Image, Label, PhotoImage, colorchooser,
+from tkinter import (CENTER, LEFT, NW, TOP, Button,  Label, Menu, PhotoImage, colorchooser,
                      filedialog, messagebox)
 from tkinter.messagebox import NO, askyesno
-from PIL import ImageGrab
-
+from turtle import width
+from PIL import ImageGrab, Image
 
 class Main:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.width = 1
+        self.fillColorValue = ""
         self.coordinates = []
         self.color = "black"
         self.root = tkinter.Tk()
+        self.buttonFrame = tkinter.Frame(self.root)
+        self.buttonFrame.pack(side=TOP)
         self.root.iconphoto(True, tkinter.PhotoImage(file="paint.png"))
         self.root.title("Aplikasi Paint")
         self.root.state('zoomed')
@@ -22,6 +26,12 @@ class Main:
         self.createCanvas()
         self.drawPen()
         self.root.mainloop()
+
+    def createButton(self, pathImage, command):
+        iconImage = PhotoImage(file=pathImage)
+        buttonAddImage = Button(self.buttonFrame, image=iconImage, command=command)
+        buttonAddImage.image = iconImage
+        buttonAddImage.pack(side=LEFT,padx=10)
 
     def addMenu(self):
         self.menu = tkinter.Menu(self.root)
@@ -34,31 +44,35 @@ class Main:
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="Exit", command=self.root.quit)
 
-        # add image menu
-        self.imageMenu = tkinter.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Add Image", command=self.addImage)
+        self.createButton("image\image.png", self.addImage)
+        self.createButton("image\pen.png", self.drawPen)
+        self.createButton("image\line.png", self.drawLine)
+        self.createButton("image\circle.png", self.drawCircle)
+        self.createButton("image\oval.png", self.drawOval)
+        self.createButton(r"image\rectangle.png", self.drawRectangle)
+        self.createButton(r"image\triangle.png", self.drawTriangle)
 
-        self.insertMenu = tkinter.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Insert", menu=self.insertMenu)
-        self.insertMenu.add_command(label="Line", command=self.drawLine)
-        self.insertMenu.add_command(label="Pen", command=self.drawPen)
-        self.insertMenu.add_command(label="Circle", command=self.drawCircle)
-        self.insertMenu.add_command(label="Oval", command=self.drawOval)
-        self.insertMenu.add_command(
-            label="Rectangle", command=self.drawRectangle)
 
-        # menu color picker
-        self.colorMenu = tkinter.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Color", command=self.setColor)
-
-        self.colorMenu = tkinter.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Fill Color", command=self.fillColor)
+        self.createButton("image\color pick.png", self.setColor)
+        self.createButton(r"image\fill color.png", self.fillColor)
 
         # menu transform 2d
         self.transformMenu = tkinter.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Transform", menu=self.transformMenu)
         self.transformMenu.add_command(label="Move", command=self.move)
         self.transformMenu.add_command(label="Scale", command=self.scale)
+        self.transformMenu.add_command(label="Rotate", command=self.rotate)
+
+        self.styleMenu = tkinter.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="Style", menu=self.styleMenu)
+
+        fillColorMenu = Menu(self.styleMenu, tearoff=0)
+        fillColorMenu.add_cascade(label="Transparent", command=lambda: self.pickColorFill(""))
+        fillColorMenu.add_cascade(label="Pick Color", command=self.pickColorFill)
+
+        
+        self.styleMenu.add_cascade(label="Fill Color", menu=fillColorMenu)
+
 
     def createCanvas(self):
         self.canvas = tkinter.Canvas(
@@ -85,7 +99,7 @@ class Main:
         if filepath == None:
             return
         ImageGrab.grab(bbox=(
-            self.root.winfo_rootx(), self.root.winfo_rooty(), self.root.winfo_rootx() + self.root.winfo_width() + 100, self.root.winfo_rooty() + self.root.winfo_height())).save(filepath)
+            self.root.winfo_rootx(), self.root.winfo_rooty()+70, self.root.winfo_rootx() + self.root.winfo_width() + 500, self.root.winfo_rooty() + self.root.winfo_height() + 200)).save(filepath)
         messagebox.showinfo("Saved", "Saved to " + filepath)
 
     def setMousePosition(self, event):
@@ -94,7 +108,7 @@ class Main:
 
     def drawLine(self):
         self.lineId = self.canvas.create_line(
-            0, 0, 0, 0, fill=self.color, width=1)
+            0, 0, 0, 0, fill=self.color, width=self.width)
         self.canvas.bind("<Button-1>", self.setMousePosition)
         self.canvas.bind("<B1-Motion>", self.drawLineMotion)
         self.canvas.bind("<ButtonRelease-1>", self.drawLineStop)
@@ -126,7 +140,7 @@ class Main:
         self.canvas.unbind("<ButtonRelease-1>")
 
         self.penId = self.canvas.create_oval(
-            0, 0, 0, 0, fill=self.color, outline=self.color, width=1)
+            0, 0, 0, 0, fill=self.color, outline=self.color, width=self.width)
         self.canvas.bind("<Button-1>", self.setMousePosition)
         self.canvas.bind("<B1-Motion>", self.drawPenMotion)
 
@@ -236,8 +250,12 @@ class Main:
                 d2 += dx - dy + rx * rx
 
     def drawRectangle(self):
+        self.canvas.unbind("<Button-1>")
+        self.canvas.unbind("<B1-Motion>")
+        self.canvas.unbind("<ButtonRelease-1>")
+
         self.rectangleId = self.canvas.create_rectangle(
-            0, 0, 0, 0, outline=self.color)
+            0, 0, 0, 0, outline=self.color, fill=self.fillColorValue)
         self.canvas.bind("<Button-1>", self.setMousePosition)
         self.canvas.bind("<B1-Motion>", self.drawRectangleMotion)
         self.canvas.bind("<ButtonRelease-1>", self.drawRectangleStop)
@@ -247,8 +265,46 @@ class Main:
 
     def drawRectangleStop(self, event):
         self.canvas.create_rectangle(
-            self.x, self.y, event.x, event.y, outline=self.color, width=1)
+            self.x, self.y, event.x, event.y, outline=self.color, width=self.width, fill=self.pick)
         self.canvas.delete(self.rectangleId)
+
+    def drawTriangle(self):
+        self.canvas.unbind("<Button-1>")
+        self.canvas.unbind("<B1-Motion>")
+        self.canvas.unbind("<ButtonRelease-1>")
+
+        self.triangleId = self.canvas.create_polygon(
+            0, 0, 0, 0, 0, 0, outline=self.color, fill=self.fillColorValue)
+        self.canvas.bind("<Button-1>", self.setMousePosition)
+        self.canvas.bind("<B1-Motion>", self.drawTriangleMotion)
+        self.canvas.bind("<ButtonRelease-1>", self.drawTriangleStop)
+
+    def drawTriangleMotion(self, event):
+
+        x1 = self.x
+        y1 = self.y
+
+        x2 = event.x
+        y2 = event.y
+
+        x3 = (x1 + x2) / 2 + math.sqrt(3) / 2 * (y2 - y1)
+        y3 = (y1 + y2) / 2 - math.sqrt(3) / 2 * (x2 - x1)
+
+        self.canvas.coords(self.triangleId, x1, y1, x2, y2, x3, y3)
+
+    def drawTriangleStop(self, event):
+        x1 = self.x
+        y1 = self.y
+
+        x2 = event.x
+        y2 = event.y
+
+        x3 = (x1 + x2) / 2 + math.sqrt(3) / 2 * (y2 - y1)
+        y3 = (y1 + y2) / 2 - math.sqrt(3) / 2 * (x2 - x1)
+
+        self.canvas.create_polygon(
+            x1, y1, x2, y2, x3, y3, outline=self.color, width=self.width, fill=self.fillColorValue)
+        self.canvas.delete(self.triangleId)
 
     def fillColor(self):
         self.canvas.unbind("<Button-1>")
@@ -274,7 +330,6 @@ class Main:
                 queuePixel.put((x + 1, y))
                 queuePixel.put((x, y - 1))
                 queuePixel.put((x, y + 1))
-        
 
     def setColor(self):
         # color picker
@@ -309,6 +364,31 @@ class Main:
         self.x = event.x
         self.y = event.y
 
+    def rotate(self):
+        self.canvas.unbind("<Button-1>")
+        self.canvas.unbind("<B1-Motion>")
+        self.canvas.unbind("<ButtonRelease-1>")
+
+        self.canvas.bind("<Button-1>", self.setMousePosition)
+        self.canvas.bind("<ButtonRelease-1>", self.rotateStop)
+
+    def rotateStop(self, event):
+        element = self.canvas.find_withtag("current")
+
+        # get height and width of element
+        x1, y1, x2, y2 = self.canvas.bbox(element)
+        
+        width = x2 - x1
+        height = y2 - y1
+
+        # get center of element
+        x = (x1 + x2) / 2
+        y = (y1 + y2) / 2
+
+        # rotate element
+        self.canvas.coords(element, x - height / 2, y -
+                           width / 2, x + height / 2, y + width / 2)
+
     def addImage(self):
         image = filedialog.askopenfilename(filetypes=(
             ("PNG files", "*.png"), ("All files", "*.*")))
@@ -320,6 +400,17 @@ class Main:
         labelImage = Label(self.root, text="Image")
         labelImage.image = tk_image
         labelImage.pack(side=LEFT)
+
+    def setWidth(self, width):
+        self.width = width
+
+    def pickColorFill(self, color=None):
+        if color is None:
+            color = colorchooser.askcolor()[1]
+        else:
+            color = ""
+
+        self.fillColorValue = color
 
 
 Main()
